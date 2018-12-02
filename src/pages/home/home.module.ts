7ebacbba28dd8a0b35a458816c910d42a3a584ac
masahiro hosoya <hosoya1985@gmail.com>
2018-12-02 20:43:20 +0900

    add rest & article

---
+++ src/pages/home/home.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { HomePage } from './home';
+
+@NgModule({
+  imports: [IonicPageModule.forChild(HomePage)],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { IonicPage, NavController, LoadingController } from 'ionic-angular';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  posts: {
+    ID: number;
+    title: string;
+    content: string;
+    date: string;
+  }[] = [];
+  constructor(
+    public navCtrl: NavController,
+    public http: HttpClient,
+    public loadingCtrl: LoadingController
+  ) {}
 
-  constructor(public navCtrl: NavController) {
-
+  ionViewDidLoad() {
+    const loading = this.loadingCtrl.create();
+    loading.present();
+    this.http
+      .get(
+        'https://public-api.wordpress.com/rest/v1.1/sites/ionicjp.wordpress.com/posts/'
+      )
+      .subscribe(data => {
+        this.posts = data['posts'];
+        loading.dismiss();
+      });
   }
-
 }
